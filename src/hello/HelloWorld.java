fe90d048f6b3c1f49f3f9c6be4c4454ba317a989
chenyuchuan <917148007@qq.com>
2025-06-04 17:02:44 +0800

    Initial commit

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class HelloWorld {
+
+
+    // 1、每个单元（字符串处理、for循环联系、文件处理可以分别在src目录下建不同的文件夹（new -> Package），然后建java文件）
+    // 2、编译后的代码统一默认会放在out文件夹中区分
+    // 3、后续可以花一点时间整理一下你已有的java文件，统一按照这种方式排列
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+}
